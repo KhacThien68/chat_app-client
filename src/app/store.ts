c443094b58aa11c64a200801c880368a38a99c53
ThienNK <thiennk@ominext.com>
2023-05-17 17:40:37 +0700

    add redux saga to redux toolkit

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,8 +1,37 @@
-import { configureStore } from '@reduxjs/toolkit'
+import {
+  Action,
+  combineReducers,
+  configureStore,
+  ThunkAction,
+} from '@reduxjs/toolkit'
+import createSagaMiddleware from 'redux-saga'
+import rootSaga from './rootSaga'
 
+// let sagaMiddleware = createSagaMiddleware()
+// const middleware = [sagaMiddleware]
+// export const store = configureStore({
+//   reducer: {},
+//   middleware: (getDefaultMiddleware) =>
+//     getDefaultMiddleware().concat(middleware),
+// })
+
+const rootReducer = combineReducers({})
+
+const sagaMiddleware = createSagaMiddleware()
 export const store = configureStore({
-  reducer: {},
+  reducer: rootReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(sagaMiddleware),
 })
+
+sagaMiddleware.run(rootSaga)
+
+export type AppThunk<ReturnType = void> = ThunkAction<
+  ReturnType,
+  RootState,
+  unknown,
+  Action<string>
+>
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>
