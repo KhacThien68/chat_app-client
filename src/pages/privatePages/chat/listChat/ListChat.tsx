48b5308562043dd09061e335a6459d06d21aca69
ThienNK <thiennk@ominext.com>
2023-05-30 17:46:40 +0700

    fix scroll

--- src/pages/privatePages/chat/listChat/ListChat.tsx
+++ src/pages/privatePages/chat/listChat/ListChat.tsx
@@ -1,29 +1,38 @@
 import { styled } from 'styled-components'
 import tw from 'twin.macro'
-import { Layout, Menu } from 'antd'
 import ChatItem from './chatItem'
-
-const { Sider: ASider } = Layout
 
 const ListChat: React.FC = () => {
   return (
     <Container>
       <Title>Trò chuyện</Title>
       <ListContainer>
-        <List>
-          <ChatItem name="Khắc Thiện" />
-          <ChatItem name="Khắc Thiện" />
-          <ChatItem name="Khắc Thiện" />
-          <ChatItem name="Khắc Thiện" />
-          <ChatItem name="Khắc Thiện" />
-          <ChatItem name="Khắc Thiện" />
-          <ChatItem name="Khắc Thiện" />
-          <ChatItem name="Khắc Thiện" />
-          <ChatItem name="Khắc Thiện" />
-          <ChatItem name="Khắc Thiện" />
-          <ChatItem name="Khắc Thiện" />
-          <ChatItem name="Khắc Thiện" />
-        </List>
+        <ChatItem name="Khắc Thiện" />
+        <ChatItem name="Khắc Thiện" />
+        <ChatItem name="Khắc Thiện" />
+        <ChatItem name="Khắc Thiện" />
+        <ChatItem name="Khắc Thiện" />
+        <ChatItem name="Khắc Thiện" />
+        <ChatItem name="Khắc Thiện" />
+        <ChatItem name="Khắc Thiện" />
+        <ChatItem name="Khắc Thiện" />
+        <ChatItem name="Khắc Thiện" />
+        <ChatItem name="Khắc Thiện" />
+        <ChatItem name="Khắc Thiện" />
+        <ChatItem name="Khắc Thiện" />
+        <ChatItem name="Khắc Thiện" />
+        <ChatItem name="Khắc Thiện" />
+        <ChatItem name="Khắc Thiện" />
+        <ChatItem name="Khắc Thiện" />
+        <ChatItem name="Khắc Thiện" />
+        <ChatItem name="Khắc Thiện" />
+        <ChatItem name="Khắc Thiện" />
+        <ChatItem name="Khắc Thiện" />
+        <ChatItem name="Khắc Thiện" />
+        <ChatItem name="Khắc Thiện" />
+        <ChatItem name="Khắc Thiện" />
+        <ChatItem name="Khắc Thiện" />
+        <ChatItem name="Khắc Thiện" />
       </ListContainer>
     </Container>
   )
@@ -32,15 +41,11 @@ const ListChat: React.FC = () => {
 export default ListChat
 
 const Container = styled.div`
-  ${tw`p-2 pt-0 flex flex-1 flex-col`}
+  ${tw`p-2 min-h-0 pt-0 flex flex-1 flex-col`}
 `
 
 const Title = styled.h6``
 
 const ListContainer = styled.div`
-  ${tw`h-full`}
-`
-
-const List = styled.div`
   ${tw`h-full overflow-y-scroll`}
 `
